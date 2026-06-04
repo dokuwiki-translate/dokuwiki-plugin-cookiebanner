a1d37cdaf57e996319c3926fa7662cd8eb409919
Wizzard <wizzardsk@gmail.com>
2026-06-04 21:30:42 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['script']                = 'Kód skriptu, ktorý sa vykoná po udelení súhlasu používateľom.';
